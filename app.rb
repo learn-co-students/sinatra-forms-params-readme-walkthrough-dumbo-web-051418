0a61531007dc056a13567307fa90b63d691c6210
github username <github email address>
2018-06-05 17:11:26 -0400

    done

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
   get '/food_form' do
     erb :food_form
+  end
+
+  post '/food' do
+    @food = params["favorite_food"]
+    @name = params["name"]
+    # binding.pry
+    "My name is #{@name}, and I love #{@food}"
   end
 
   # Add your post route and action below
